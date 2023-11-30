8f5bb8c327f771a9fabf966ab3ad4bdf3dab2df4
Malichi Hill <99568086+MalichiD@users.noreply.github.com>
2023-11-30 13:29:58 -0500

    Updated Dependencies in prep for implementation of web sockets and chat functionality.

--- types.ts
+++ types.ts
@@ -1,5 +1,17 @@
+import {Server as NetServer, Socket} from "net";
+import {NextApiResponse} from "next";
+import {Server as SocketIOServer} from "socket.io"
+
 import {Server, Member, Profile, BannedUser} from "@prisma/client"
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })[];
     bannedUsers: (BannedUser & { profile: Profile })[];
 }
+
+export type NextApiResponseServerIo = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIOServer;
+        };
+    };
+};
