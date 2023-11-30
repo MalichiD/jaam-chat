b80e5a41b97a439eadd3b151cff3dc97e0ab3b65
Malichi Hill <99568086+MalichiD@users.noreply.github.com>
2023-11-30 13:43:54 -0500

    Deploy

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -41,7 +41,7 @@ const ChannelIdPage = async ({
   if (!channel || !member) {
     redirect("/");
   }
-
+  // Deploy
   return ( 
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader
